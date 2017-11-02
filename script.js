9137bf58feb257ff3193ae3c0aa40c5798ba5bec
Tech-A03 <tech-a03@Tech-A03s-MacBook-Air-2.local>
2017-11-02 23:11:25 +0000

    Added check function

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 
+
+
+$(function(event) {
 // function for when button is clicked
-
-
-function buttonClick() {
-  if(cross === true) {
-    $('td').click(function(){
-      $(this).html('X');
-      cross = false;
-      nought = true;
-      $('.playerTurn').html('It is O\'s turn');
-  })
-  }
-}
-
+console.log("dom");
 // global variables
 var cross = true;
-var nought = false;
+var winner = false;
+
+function buttonClick() {
+  $('td').click(function() {
+    check();
+    if(cross === true && winner === false) {
+      cross = false;
+      $(this).html('X').addClass('X');
+      $('.playerTurn').html('It is O\'s turn');
+      console.log(this);
+    } else if(winner === false){
+      cross = true;
+      $(this).html('O').addClass('O');
+      $('.playerTurn').html('It is X\'s turn');
+    }
+  });
+}
+
+// Checks for line
+function check() {
+  debugger;
+  if(($('td')[0].innerHTML === $('td')[1].innerHTML === $('td')[2].innerHTML) || ($('td')[3].innerHTML === $('td')[4].innerHTML === $('td')[5].innerHTML) || ($('td')[6].innerHTML === $('td')[7].innerHTML === $('td')[8].innerHTML) || ($('td')[0].innerHTML === $('td')[3].innerHTML === $('td')[6].innerHTML) || ($('td')[1].innerHTML === $('td')[4].innerHTML === $('td')[7].innerHTML) || ($('td')[2].innerHTML === $('td')[5].innerHTML === $('td')[8].innerHTML) || ($('td')[2].innerHTML === $('td')[4].innerHTML === $('td')[6].innerHTML) || ($('td')[0].innerHTML === $('td')[4].innerHTML === $('td')[8].innerHTML)) {
+    winner = true;
+  }
+}
 
 
 // function to start game
@@ -24,3 +39,5 @@ function beginGame() {
 }
 
 beginGame();
+
+});
